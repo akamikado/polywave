bd81f661e3040a860ce080a2329856b689d451d2
akamikado <manishvasireddy1234@gmail.com>
2025-11-08 21:54:37 +0530

    added audio callback to get frames from song

--- src/game.c
+++ src/game.c
@@ -1,22 +1,36 @@
 #include "game.h"
 #include <raylib.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct {
-  Wave w;
+  char *song;
+  Wave wave;
+  Music music;
 } State;
 
 static State *s = NULL;
 
+static void audio_callback(void *bufferData, unsigned int frames) {}
+
 void game_init() {
   s = malloc(sizeof(State));
   InitAudioDevice();
-  s->w = LoadWave("build/Insane-Gameplay.mp3");
-  Sound music = LoadSoundFromWave(s->w);
-  PlaySound(music);
+  s->song = strdup("build/Insane-Gameplay.mp3");
+  s->wave = LoadWave(s->song);
+  s->music = LoadMusicStream(s->song);
+  PlayMusicStream(s->music);
+  AttachAudioStreamProcessor(s->music.stream, audio_callback);
 }
 
-void game_update() { ClearBackground(BLUE); }
+void game_update() {
+  ClearBackground(BLACK);
+  if (IsMusicStreamPlaying(s->music)) {
+    UpdateMusicStream(s->music);
+
+    float *data = LoadWaveSamples(s->wave);
+  }
+}
 
 void *game_pre_reload() { return s; }
 
