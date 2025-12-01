899e51a1761cb198650005f171133a35268510b1
akamikado <manishvasireddy1234@gmail.com>
2025-12-02 02:11:40 +0530

    added windows platform check

--- nob.c
+++ nob.c
@@ -15,21 +15,35 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  cmd_append(&cmd, "cc");
+  #ifdef __WIN32
+  cmd_append(&cmd, "x86_64-w64-mingw32-gcc");
   cmd_append(&cmd, "-Wall", "-Wextra");
-  cmd_append(&cmd, "-I./raylib-5.5_linux_amd64/include/");
-  cmd_append(&cmd, "-g");
-  cmd_append(&cmd, "-o", BUILD_FOLDER "main");
+  cmd_append(&cmd, "-mwindows");
+  cmd_append(&cmd, "-I./raylib-5.5_win32_mingw-w64/include/");
+  cmd_append(&cmd, "-o", BUILD_FOLDER "polywave.exe");
   cmd_append(&cmd, SRC_FOLDER "main.c");
   cmd_append(&cmd, SRC_FOLDER "game.c");
-  cmd_append(&cmd, "-Wl,-rpath=./raylib-5.5_linux_amd64/lib/");
-  cmd_append(&cmd, "-L./raylib-5.5_linux_amd64/lib/");
-  cmd_append(&cmd, "-l:libraylib.so");
+  cmd_append(&cmd, "-L./raylib-5.5_win32_mingw-w64/lib/");
+  cmd_append(&cmd, "-lraylib");
   cmd_append(&cmd, "-lm");
   if (!cmd_run(&cmd)) {
     fprintf(stderr, "Failed creating main executable\n");
     return 1;
   }
+  #else
+  cmd_append(&cmd, "cc");
+  cmd_append(&cmd, "-Wall", "-Wextra");
+  cmd_append(&cmd, "-I./raylib-5.5_linux_amd64/include/");
+  cmd_append(&cmd, "-o", BUILD_FOLDER "polywave");
+  cmd_append(&cmd, SRC_FOLDER "main.c");
+  cmd_append(&cmd, SRC_FOLDER "game.c");
+  cmd_append(&cmd, "./raylib-5.5_linux_amd64/lib/libraylib.a");
+  cmd_append(&cmd, "-lm");
+  if (!cmd_run(&cmd)) {
+    fprintf(stderr, "Failed creating main executable\n");
+    return 1;
+  }
+  #endif
 
   return 0;
 }
