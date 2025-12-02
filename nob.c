bc15c6f0a8a4d4aec35c5d4d44424d9e9227cbb9
akamikado <manishvasireddy1234@gmail.com>
2025-12-02 12:11:49 +0530

    added macos raylib release and nob changes

--- nob.c
+++ nob.c
@@ -103,16 +103,18 @@ int main(int argc, char **argv) {
   cmd_append(&cmd, "-lraylib");
   cmd_append(&cmd, "-lm");
   cmd_append(&cmd, "-lwinmm");
-  if (!cmd_run(&cmd)) {
-    fprintf(stderr, "Failed creating main executable\n");
-    return 1;
-  }
   #else
   #ifdef MACOS
   cmd_append(&cmd, "clang");
+  cmd_append(&cmd, "-Wall", "-Wextra");
+  cmd_append(&cmd, "-I./raylib-5.5_macos/include/");
+  cmd_append(&cmd, "-o", BUILD_FOLDER "polywave-linux");
+  cmd_append(&cmd, SRC_FOLDER "main.c");
+  cmd_append(&cmd, SRC_FOLDER "game.c");
+  cmd_append(&cmd, "./raylib-5.5_macos/lib/libraylib.a");
+  cmd_append(&cmd, "-lm");
   #else
   cmd_append(&cmd, "cc");
-  #endif
   cmd_append(&cmd, "-Wall", "-Wextra");
   cmd_append(&cmd, "-I./raylib-5.5_linux_amd64/include/");
   cmd_append(&cmd, "-o", BUILD_FOLDER "polywave-linux");
@@ -120,11 +122,13 @@ int main(int argc, char **argv) {
   cmd_append(&cmd, SRC_FOLDER "game.c");
   cmd_append(&cmd, "./raylib-5.5_linux_amd64/lib/libraylib.a");
   cmd_append(&cmd, "-lm");
+  #endif
+  #endif
+
   if (!cmd_run(&cmd)) {
     fprintf(stderr, "Failed creating main executable\n");
     return 1;
   }
-  #endif
 
   return 0;
 }
