df2ffc9a914ec9504792bb347e1a3db34a6b91f8
akamikado <manishvasireddy1234@gmail.com>
2025-12-02 15:21:51 +0530

    added game title

--- src/icon.c
+++ src/icon.c
@@ -8,6 +8,7 @@ int main() {
     ClearBackground(GetColor(0xeff7f6ff));
     DrawCircleV((Vector2){.x = 200, .y = 200}, 200, GetColor(0xef946cff));
     DrawCircleV((Vector2){.x = 200, .y = 200}, 100, GetColor(0xfb232eff));
+    DrawText("POLYWAVE", 200 - MeasureText("POLYWAVE", 50) / 2, 175, 50, GetColor(0x2a0800ff));
     EndDrawing();
   }
 }
